f6576a021289a74c592e4b043f1e132fa79324eb
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-07 18:22:01 +0100

    fix misnamed variable

--- myApp.js
+++ myApp.js
@@ -100,7 +100,7 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({ food: foodToSearch })
+  Person.find({ favoriteFoods: foodToSearch })
     .sort({ name: 1 })
     .limit(2)
     .select({ name: 1, age: 0, favoriteFoods: 1 })
