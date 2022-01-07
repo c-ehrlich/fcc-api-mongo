0d7908f71d8c0e19b95807da4507f88c94338757
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-07 18:06:11 +0100

    fix queryChain to use mongoose 5.x syntax

--- myApp.js
+++ myApp.js
@@ -101,9 +101,9 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person.find({ food: foodToSearch })
-    .sort({ name: "asc" })
+    .sort({ name: 1 })
     .limit(2)
-    .select("-age")
+    .select({ name: 1, age: 0, favoriteFoods: 1 })
     .exec((err, data) => {
       if (err) return console.error(err);
       done(null, data);
